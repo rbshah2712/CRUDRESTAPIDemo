ae052ca6d544d39b49bc7adc01883d3e5d987d86
rbshah2712 <34372610+rbshah2712@users.noreply.github.com>
2026-09-24 12:03:48 -0400

    AWS deployed

--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-cd /home/ec2-user/app
+cd /home/ec2-user/CRUDRESTAPIDemo
 
 JAR_FILE=$(find . -name "*.jar" | head -n 1)
 
+echo "Starting $JAR_FILE"
+
 nohup java -jar "$JAR_FILE" > application.log 2>&1 &
+
+echo $! > application.pid
--- scripts/stop_application.sh
+++ scripts/stop_application.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-pkill -f 'java -jar' || true
+if [ -f /home/ec2-user/CRUDRESTAPIDemo/application.pid ]; then
+
+    PID=$(cat /home/ec2-user/CRUDRESTAPIDemo/application.pid)
+
+    if kill -0 "$PID" 2>/dev/null; then
+        kill "$PID"
+    fi
+
+    rm -f /home/ec2-user/CRUDRESTAPIDemo/application.pid
+fi
+
+exit 0
